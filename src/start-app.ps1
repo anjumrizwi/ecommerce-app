b81734d255326b60f97ae6720085a9d10136c2e7
anjumrizwi <anjumrizwi@gmail.com>
2026-03-27 10:35:05 +0530

    add start-app.ps1

---
+++ src/start-app.ps1
@@ -0,0 +1,18 @@
+﻿# Base path
+$BASE_PATH = "D:\GitHub\anjumrizwi\ecommerce-app\src"
+
+Write-Host "Starting Ecommerce API..." -ForegroundColor Cyan
+
+Start-Process powershell -ArgumentList @"
+cd '$BASE_PATH'
+dotnet run --project Ecommerce.API/Ecommerce.API.csproj
+"@
+
+Write-Host "Starting React UI..." -ForegroundColor Cyan
+
+Start-Process powershell -ArgumentList @"
+cd '$BASE_PATH\Ecommerce.UI\ClientApp'
+npm run dev
+"@
+
+Write-Host "All services started successfully." -ForegroundColor Green
